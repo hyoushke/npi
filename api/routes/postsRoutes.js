ab6d85a79a3b070ea5aaf981a00023d7cd2f4e54
hyoushke <hyoushke@gmail.com>
2019-05-27 04:40:09 +0800

    Optimize postsRoutes.js

--- api/routes/postsRoutes.js
+++ api/routes/postsRoutes.js
@@ -2,25 +2,14 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-
-//Controllers
 const PostsController = require('../controllers/postsController');
-
-
 
 router.post('/', PostsController.createPost);
 router.patch('/:postId', PostsController.updatePost);
 router.delete('/:postId', PostsController.removePost);
 
-
 router.get('/mail', PostsController.sendMailTest);
 router.get('/:postId', PostsController.getPost);
 router.get('/:limit/:page/:field/:value', PostsController.listPosts);
 
-//Tests
-
-
-
-
 module.exports = router;
-
