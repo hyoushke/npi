d6fba29ab93424807baf4f2aecd69a5c6df51ea3
hyoushke <hyoushke@gmail.com>
2019-05-27 04:51:45 +0800

    Created new mongoose helper / utils file to only call it once in the models

--- api/models/postsModel.js
+++ api/models/postsModel.js
@@ -1,12 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('../utils/mongooseUtils');
 const mongoosePaginate = require('mongoose-paginate-v2');
-
-const {DB_MONGO_USER, DB_MONGO_PASSWORD, DB_MONGO_HOST, DB_MONGO_DATABASE } = process.env;
-
-const muri = `mongodb+srv://${DB_MONGO_USER}:${DB_MONGO_PASSWORD}@${DB_MONGO_HOST}/${DB_MONGO_DATABASE}?retryWrites=true`;
-mongoose.connect(muri);
-mongoose.Promise = global.Promise;
-
 
 const postsSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
---
+++ api/utils/mongooseUtils.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const {DB_MONGO_USER, DB_MONGO_PASSWORD, DB_MONGO_HOST, DB_MONGO_DATABASE } = process.env;
+const muri = `mongodb+srv://${DB_MONGO_USER}:${DB_MONGO_PASSWORD}@${DB_MONGO_HOST}/${DB_MONGO_DATABASE}?retryWrites=true`;
+mongoose.connect(muri);
+mongoose.Promise = global.Promise;
+
+exports.mongoose = mongoose;
