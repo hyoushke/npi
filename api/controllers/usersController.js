61aa13fdeec381a2ebf78f712b9495bf9fd4d998
hyoushke <hyoushke@gmail.com>
2019-05-28 13:23:19 +0800

    Modified usersController added codes for loginUser

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,44 +1,63 @@
 const BenchmarkUtils = require('../utils/benchmarkUtils');
 const UsersService = require('../services/usersServices');
+const UsersResponse = require('../response/genericResponse');
 
 
 exports.creteUser = async (req, res, next)=>{
     try
     {
-
         const benchmark = new BenchmarkUtils();
         const status = "Active"; 
         const username = req.body.username; 
-        const firstname = req.body.firstname; 
-        const lastname = req.body.lastname; 
         const email = req.body.email; 
         const password = req.body.password; 
+        const firstname = req.body.firstname; 
+        const lastname = req.body.lastname; 
         const gender = ''; 
         const avatar = ''; 
         const datecreated = Date.now();
         const datemodified = Date.now();
-        const usersServiceData = await UsersService.createUser(status, 
-                                                                postid, 
-                                                                userid, 
-                                                                comment, 
-                                                                datecreated, 
-                                                                datemodified);
-        if(commentsServiceData.error) 
+
+        const usersServiceData = await UsersService.createUser(status, username, email, password, firstname, lastname, gender, avatar, datecreated, datemodified);
+
+        if(usersServiceData.error) 
         {
-            throw (commentsServiceData.error);
+            throw (usersServiceData.error);
         }
 
-        const r = CommentsResponse.genericResponse('users', 'create', 'success', usersServiceData, benchmark.getDuration());
+        const r = UsersResponse.genericResponse('users', 'create', 'success', usersServiceData, benchmark.getDuration());
 
         res.status(r.code).json(r);
     }
     catch(error)
     {
-        const r = CommentsResponse.genericResponse('users', 'create', 'failed', error, '0 ms');
+        const r = UsersResponse.genericResponse('users', 'create', 'failed', error, '0 ms');
         res.status(r.code).json(r);
     }
 }
 
 exports.loginUser = async (req, res, next)=> {
+
+    try
+    {
+        const usersServiceData = await UsersService.loginUser(email, password);
+        
+        if(usersServiceData.error) 
+        {
+            throw (usersServiceData.error);
+        }
+
+        const r = CommentsResponse.genericResponse('users', 'create', 'success', usersServiceData, benchmark.getDuration());
+
+        res.status(r.code).json(r);
+
+    }
+    catch(error)
+    {
+        const r = usersServiceData.genericResponse('users', 'login', 'failed', error, '0 ms');
+        res.status(r.code).json(r);
+    }
+    
+
 
 }
